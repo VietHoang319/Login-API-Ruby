43ed96f7b913b636d8efe667991a9c81577ef059
hoangnv <hoangnv@ai-t.vn>
2023-08-24 16:46:56 +0700

    update swagger

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -7,9 +7,14 @@ class CurrentUserController < ApplicationController
   def update_password
     @user = User.find(current_user.id)
     if @user.update_with_password(user_password_params)
-      render json: {code: 200, message: "Change password sucessfully."}, status: :ok
+      render json: {
+        status: {code: 200, message: "Change password sucessfully."},
+        data: UserSerializer.new(@user).serializable_hash[:data][:attributes]
+      } , status: :ok
     else
-      render json: :error
+      render json: {
+        status: {code: 400, message: "#{@user.errors.full_messages.to_sentence}"}
+      }, status: :bad_request
     end
   end
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -69,10 +69,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
         status: {code: 200, message: "Signed up sucessfully."},
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }, status: :ok
-    elsif request.method == "DELETE"
-      render json: {
-        status: { code: 200, message: "Account deleted successfully."}
-      }, status: :ok
     else
       render json: {
         status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
